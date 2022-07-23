502909641bb91f8d2bfb7761823f17cef62b7319
João Pedro <joaoplay16@gmail.com>
2022-07-23 19:05:00 -0300

    English language added

--- app/src/main/java/com/playlab/bussolaagil/util/Directions.kt
+++ app/src/main/java/com/playlab/bussolaagil/util/Directions.kt
@@ -1,15 +1,20 @@
 package com.playlab.bussolaagil.util
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import com.playlab.bussolaagil.R
+
+@Composable
 fun getDirectionsLabel(degrees: Int):String{
     return when (degrees) {
-        in 338 .. 360, in 0 .. 22 -> "N"
-        in 23 .. 67 -> "NE"
-        in 68 .. 112 -> "L"
-        in 113 .. 157 -> "SE"
-        in 158 .. 202 -> "S"
-        in 203 .. 247 -> "SO"
-        in 248 .. 292 -> "O"
-        in 293 .. 337 -> "NO"
+        in 338 .. 360, in 0 .. 22 -> stringResource(R.string.north)
+        in 23 .. 67 -> stringResource(R.string.northeast)
+        in 68 .. 112 -> stringResource(R.string.east)
+        in 113 .. 157 -> stringResource(R.string.southeast)
+        in 158 .. 202 -> stringResource(R.string.south)
+        in 203 .. 247 -> stringResource(R.string.southwest)
+        in 248 .. 292 -> stringResource(R.string.west)
+        in 293 .. 337 -> stringResource(R.string.northwest)
         else -> ""
     }
 }
