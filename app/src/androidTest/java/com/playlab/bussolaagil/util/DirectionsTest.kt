f38cecbbe76991d0c24fadfcffd139ec987199d8
João Pedro <joaoplay16@gmail.com>
2022-10-21 21:22:45 -0300

    improve diretions test for support i18n

--- app/src/androidTest/java/com/playlab/bussolaagil/util/DirectionsTest.kt
+++ app/src/androidTest/java/com/playlab/bussolaagil/util/DirectionsTest.kt
@@ -4,70 +4,89 @@ import android.content.Context
 import androidx.test.platform.app.InstrumentationRegistry
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import com.playlab.bussolaagil.R
 
 class DirectionsTest {
     private val appContext: Context = InstrumentationRegistry.getInstrumentation().targetContext
 
     @Test
     fun degree_between_0_and_22_should_return_North() {
-        for (deg in 0..22){
-            assertEquals(getDirectionsLabel(appContext, deg), "N")
+        with(appContext){
+            for (deg in 0..22){
+                assertEquals(getDirectionsLabel(this, deg), getString(R.string.north))
+            }
         }
     }
 
     @Test
     fun degree_between_338_and_360_should_return_North() {
-        for (deg in 338..360) {
-            assertEquals(getDirectionsLabel(appContext, deg), "N")
+        with(appContext){
+            for (deg in 338..360) {
+                assertEquals(getDirectionsLabel(this, deg), getString(R.string.north))
+            }
         }
     }
 
     @Test
     fun degree_between_23_and_67_should_return_Northeast() {
-        for (deg in 23..67) {
-            assertEquals(getDirectionsLabel(appContext, deg), "NE")
+        with(appContext){
+            for (deg in 23..67) {
+                assertEquals(getDirectionsLabel(this, deg), getString(R.string.northeast))
+            }
         }
     }
 
     @Test
     fun degree_between_68_and_112_should_return_East() {
-        for (deg in 68..112) {
-            assertEquals(getDirectionsLabel(appContext, deg), "L")
+        with(appContext){
+            for (deg in 68..112) {
+                assertEquals(getDirectionsLabel(this, deg), getString(R.string.east))
+            }
         }
     }
 
     @Test
     fun degree_between_113_and_157_should_return_Southeast() {
-        for (deg in 113..157) {
-            assertEquals(getDirectionsLabel(appContext, deg), "SE")
+        with(appContext){
+            for (deg in 113..157) {
+                assertEquals(getDirectionsLabel(this, deg), getString(R.string.southeast))
+            }
         }
     }
 
     @Test
     fun degree_between_158_and_202_should_return_South() {
-        for (deg in 158..202) {
-            assertEquals(getDirectionsLabel(appContext, deg), "S")
+        with(appContext){
+            for (deg in 158..202) {
+                assertEquals(getDirectionsLabel(this, deg), getString(R.string.south))
+            }
         }
     }
 
     @Test
     fun degree_between_203_and_247_should_return_Southwest() {
-        for (deg in 203..247) {
-            assertEquals(getDirectionsLabel(appContext, deg), "SO")
+        with(appContext){
+            for (deg in 203..247) {
+                assertEquals(getDirectionsLabel(this, deg), getString(R.string.southwest))
+            }
         }
     }
 
     @Test
     fun degree_between_248_and_292_should_return_West() {
-        for (deg in 248..292) {
-            assertEquals(getDirectionsLabel(appContext, deg), "O")
+        with(appContext){
+            for (deg in 248..292) {
+                assertEquals(getDirectionsLabel(this, deg), getString(R.string.west))
+            }
         }
     }
 
     @Test
     fun degree_between_293_and_337_should_return_NorthWest() {
-        for (deg in 293..337) {
-            assertEquals(getDirectionsLabel(appContext, deg), "NO")
+        with(appContext){
+            for (deg in 293..337) {
+                assertEquals(getDirectionsLabel(this, deg), getString(R.string.northwest))
+            }
         }
     }
 }
