079e888411917e540c4461e0abb3ca15aa53f5b9
João Pedro <joaoplay16@gmail.com>
2022-11-11 20:39:56 -0300

    refact: create base compass composable widget the rotation logic is placed in the BaseCompass composable the widgets variants are just responsible for the styling.

---
+++ app/src/main/java/com/playlab/bussolaagil/util/Constants.kt
@@ -0,0 +1,9 @@
+package com.playlab.bussolaagil.util
+
+object Constants {
+
+    /**
+     * Value that represents the compass needle centered on top position.
+    **/
+    const val COMPASS_CENTER_TOP_POSITION = 270
+}
