794a644a79a2c6f50aa6637e580b04358f0a849f
João Pedro <joaoplay16@gmail.com>
2022-11-12 22:26:29 -0300

    test error dialog display when magnetic field sensor is not present

--- app/src/androidTest/java/com/playlab/bussolaagil/screens/TestHomeScreen.kt
+++ app/src/androidTest/java/com/playlab/bussolaagil/screens/TestHomeScreen.kt
@@ -1,22 +1,48 @@
 package com.playlab.bussolaagil.screens
 
+import android.content.Context
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.performClick
 import androidx.navigation.compose.ComposeNavigator
 import androidx.navigation.testing.TestNavHostController
+import androidx.test.platform.app.InstrumentationRegistry
 import com.playlab.bussolaagil.DefaultNavHost
+import com.playlab.bussolaagil.R
+import com.playlab.bussolaagil.screens.home.HomeScreen
 import com.playlab.bussolaagil.ui.theme.BussolaAgilTheme
 import org.junit.Rule
 import org.junit.Test
 
 class TestHomeScreen {
+    private val appContext: Context = InstrumentationRegistry.getInstrumentation().targetContext
 
     @get:Rule
     val composeTestRule = createComposeRule()
 
     lateinit var navController: TestNavHostController
+
+    @Test
+    fun showErrorMessageDialog_whenMagneticSensorFieldIsNotPresent(){
+
+        composeTestRule.setContent {
+            BussolaAgilTheme {
+                navController = TestNavHostController(LocalContext.current)
+                navController.navigatorProvider.addNavigator(ComposeNavigator())
+
+                HomeScreen(
+                    degrees = 0,
+                    isMagneticFieldSensorPresent = false,
+                    navController = navController) {}
+            }
+        }
+
+        val expectedText = appContext.getString(R.string.missing_sensor_error)
+
+        composeTestRule.onNode(hasText(expectedText)).assertExists()
+    }
 
     @Test
     fun clickOptionsMenu_navigateToWidgetScreen(){
