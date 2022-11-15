8047476b2250948d899f03c9a0d4d54779b57f09
João Pedro <joaoplay16@gmail.com>
2022-11-14 21:38:36 -0300

    test navigation from widget selection screen to home screen

---
+++ app/src/androidTest/java/com/playlab/bussolaagil/screens/widget/TestWidgetScreen.kt
@@ -0,0 +1,48 @@
+package com.playlab.bussolaagil.screens.widget
+
+import android.content.Context
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.performClick
+import androidx.navigation.compose.ComposeNavigator
+import androidx.navigation.testing.TestNavHostController
+import androidx.test.platform.app.InstrumentationRegistry
+import com.playlab.bussolaagil.DefaultNavHost
+import com.playlab.bussolaagil.ui.theme.BussolaAgilTheme
+import com.playlab.bussolaagil.R
+import org.junit.Rule
+import org.junit.Test
+
+class TestWidgetScreen {
+    private val appContext: Context = InstrumentationRegistry.getInstrumentation().targetContext
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    lateinit var navController: TestNavHostController
+
+    @Test
+    fun clickArrowBack_navigateToHomeScreen(){
+
+        val widgetScreenArrowBackIconDescription = appContext.getString(R.string.back)
+        val homeScreenMenuIconDescription = appContext.getString(R.string.menu)
+
+        composeTestRule.setContent {
+            BussolaAgilTheme (){
+                navController = TestNavHostController(LocalContext.current)
+                navController.navigatorProvider.addNavigator(ComposeNavigator())
+
+               DefaultNavHost(
+                   degrees = 0,
+                   isMagneticFieldSensorPresent = true,
+               )
+            }
+        }
+
+        composeTestRule.onNodeWithContentDescription(homeScreenMenuIconDescription).performClick()
+        composeTestRule.onNodeWithContentDescription(widgetScreenArrowBackIconDescription).assertExists()
+        composeTestRule.onNodeWithContentDescription(widgetScreenArrowBackIconDescription).performClick()
+        composeTestRule.onNodeWithContentDescription(homeScreenMenuIconDescription).assertExists()
+    }
+}
