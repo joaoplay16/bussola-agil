e680e7e0a14664c377ebb58b3b3f234f53b08832
Joao Pedro <joaoplay16@gmail.com>
2025-09-25 20:29:46 -0300

    Bump app version code to 5 and version name to "1.1.1"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.playlab.bussolaagil"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 4
-        versionName = "1.1.0"
+        versionCode = 5
+        versionName = "1.1.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
